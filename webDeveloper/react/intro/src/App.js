d3992a0b7eff9cc86d63a0c34bd33cd0f0f29ba7
Omer Faruk Ozmen <omerfarukozmen0@gmail.com>
2021-10-16 12:10:01 +0300

    Getting Started with React

---
+++ webDeveloper/react/intro/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import CategoryList from "./CategoryList";
+import Navi from "./Navi";
+import ProductList from "./ProductList";
+
+function App() {
+  let productInfo = {title:"Product List"}
+  let categoryInfo = {title:"Category List"}
+  
+
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Navi />
+        </Row>
+        <Row>
+          <Col xs="3">
+            <CategoryList info={categoryInfo} />
+          </Col>
+          <Col xs="9">
+            <ProductList info={productInfo} />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default App;

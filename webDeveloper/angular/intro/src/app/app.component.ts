978c33b1ae27accc86c59f3b3844ba740db62d9f
Omer Faruk Ozmen <omerfarukozmen0@gmail.com>
2021-10-29 13:46:56 +0300

    Angular intro

---
+++ webDeveloper/angular/intro/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'intro';
+  firstName = "Ömer Faruk"
+
+  //function
+}

ab0503e061d181a88d1f14688847fd528955df7b
Omer Faruk Ozmen <omerfarukozmen0@gmail.com>
2021-10-31 15:11:23 +0300

    Adding products via API

--- webDeveloper/vue/intro/src/App.vue
+++ webDeveloper/vue/intro/src/App.vue
@@ -20,43 +20,44 @@ export default {
   },
   data() {
     return {
-      products: [
-        {
-          id: 1,
-          categoryId: 1,
-          productName: "Laptop",
-          quantityPerUnit: "Acer i5",
-          unitPrice: 5000,
-          unitsInStock: 2,
-        },
-        {
-          id: 2,
-          categoryId: 1,
-          productName: "Mouse",
-          quantityPerUnit: "Mouse i5",
-          unitPrice: 5000,
-          unitsInStock: 12,
-        },
-        {
-          id: 3,
-          categoryId: 2,
-          productName: "Keyboard",
-          quantityPerUnit: "Logitech keyboard",
-          unitPrice: 5000,
-          unitsInStock: 20,
-        },
-      ],
+      products: [],
     };
   },
+  mounted(){
+    this.getProducts()
+  },
   methods: {
-    deleteProduct(product) {
+    async getProducts() {
+      const result = await fetch("http://localhost:3000/products");
+      const data = await result.json();
+      this.products = data;
+    },
+    async deleteProduct(product) {
+      await fetch('http://localhost:3000/products/'+product.id,{
+        method:'DELETE',
+
+      })
+
       this.products = this.products.filter(
         (productToFilter) => productToFilter.id !== product.id
       );
     },
-    updateProduct() {},
-    addProduct(product) {
-      const newProduct = { ...product };
+    async updateProduct(product) {
+      const result = await fetch("http://localhost:3000/products/"+product.id,{
+        method:'PUT',
+        body:JSON.stringify(product),
+        headers:{'Content-Type':'application/json'}
+      });
+      const updatedProduct = await result.json()
+      this.products=this.products.map(product=>product.id===updatedProduct.id?updatedProduct:product)
+    },
+    async addProduct(product) {
+      const result = await fetch("http://localhost:3000/products",{
+        method:'POST',
+        body:JSON.stringify(product),
+        headers:{'Content-Type':'application/json'}
+      });
+      const newProduct = await result.json()
       this.products = [...this.products, newProduct];
     },
   },

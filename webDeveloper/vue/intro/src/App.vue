64faaa1d03e8d7b9d10b90a5206a29476e0a10cf
Omer Faruk Ozmen <omerfarukozmen0@gmail.com>
2021-10-31 14:40:06 +0300

    List products and add new products with Vue

--- webDeveloper/vue/intro/src/App.vue
+++ webDeveloper/vue/intro/src/App.vue
@@ -1,15 +1,64 @@
 <template>
   <div id="app">
-    <ProductList />
+    <ProductAdd @add:product="addProduct" />
+    <ProductList
+      @delete:product="deleteProduct"
+      @update:product="updateProduct"
+      :products="products"
+    />
   </div>
 </template>
 
 <script>
 import ProductList from "./components/ProductList.vue";
+import ProductAdd from "./components/ProductAdd.vue";
 export default {
   name: "App",
   components: {
     ProductList,
+    ProductAdd,
+  },
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          categoryId: 1,
+          productName: "Laptop",
+          quantityPerUnit: "Acer i5",
+          unitPrice: 5000,
+          unitsInStock: 2,
+        },
+        {
+          id: 2,
+          categoryId: 1,
+          productName: "Mouse",
+          quantityPerUnit: "Mouse i5",
+          unitPrice: 5000,
+          unitsInStock: 12,
+        },
+        {
+          id: 3,
+          categoryId: 2,
+          productName: "Keyboard",
+          quantityPerUnit: "Logitech keyboard",
+          unitPrice: 5000,
+          unitsInStock: 20,
+        },
+      ],
+    };
+  },
+  methods: {
+    deleteProduct(product) {
+      this.products = this.products.filter(
+        (productToFilter) => productToFilter.id !== product.id
+      );
+    },
+    updateProduct() {},
+    addProduct(product) {
+      const newProduct = { ...product };
+      this.products = [...this.products, newProduct];
+    },
   },
 };
 </script>
@@ -19,7 +68,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+
   color: #2c3e50;
   margin-top: 60px;
 }
--- webDeveloper/vue/intro/src/components/ProductList.vue
+++ webDeveloper/vue/intro/src/components/ProductList.vue
@@ -1,11 +1,128 @@
 <template>
-  <div>
-      <h3>Product List</h3>
+  <div id="list">
+    <p v-if="products.length == 0">Ürün Listesi boş</p>
+    <table v-else class="table">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Ürün adı</th>
+          <th>Kategori</th>
+          <th>Açıklama</th>
+          <th>Birim fiyatı</th>
+          <th>Stok adedi</th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="product in products" :key="product.id">
+          <td v-if="updateId === product.id">
+            <input
+              v-model="product.id"
+              type="text"
+              class="form-control"
+              id="id"
+            />
+          </td>
+          <td v-else>{{ product.id }}</td>
+          <td v-if="updateId === product.id">
+            <input
+              v-model="product.productName"
+              type="text"
+              class="form-control"
+              id="productName"
+            />
+          </td>
+          <td v-else>{{ product.productName }}</td>
+          <td v-if="updateId === product.id">
+            <input
+              v-model="product.categoryId"
+              type="text"
+              class="form-control"
+              id="categoryId"
+            />
+          </td>
+          <td v-else>{{ product.categoryId }}</td>
+          <td v-if="updateId === product.id">
+            <input
+              v-model="product.quantityPerUnit"
+              type="text"
+              class="form-control"
+              id="quantityPerUnit"
+            />
+          </td>
+          <td v-else>{{ product.quantityPerUnit }}</td>
+          <td v-if="updateId === product.id">
+            <input
+              v-model="product.unitPrice"
+              type="text"
+              class="form-control"
+              id="unitPrice"
+            />
+          </td>
+          <td v-else>{{ product.unitPrice }}</td>
+          <td v-if="updateId === product.id">
+            <input
+              v-model="product.unitsInStock"
+              type="text"
+              class="form-control"
+              id="unitsInStock"
+            />
+          </td>
+          <td v-else>{{ product.unitsInStock }}</td>
+          <td v-if="updateId !== product.id">
+            <button
+              class="btn btn-sm btn-primary"
+              @click="handleUpdate(product)"
+            >
+              Güncelle
+            </button>
+            <button
+              class="btn btn-sm btn-danger"
+              @click="handleDelete(product)"
+            >
+              Sil
+            </button>
+          </td>
+          <td v-else>
+            <button class="btn btn-sm btn-success" @click="handleSave(product)">
+              Kaydet
+            </button>
+            <button class="btn btn-sm btn-danger" @click="updateId = null">
+              İptal
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  name: "product-list",
+  data() {
+    return { updateId: null };
+  },
+  props: {
+    products: Array,
+  },
+  methods: {
+    handleDelete(product) {
+      this.$emit("delete:product", product);
+    },
+    handleUpdate(product) {
+      this.updateId = product.id;
+    },
+    handleSave(product) {
+      this.$emit("update:product", product);
+      this.updateId = null;
+    },
+  },
+};
 </script>
 
 <style scoped>
+#list {
+  margin: 3em;
+}
 </style>

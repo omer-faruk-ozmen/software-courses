54cc0c8c465dbcb848949557167d58309c9ef2af
Omer Faruk Ozmen <omerfarukozmen0@gmail.com>
2021-11-04 06:41:46 +0300

    CSharp Loops

---
+++ CSharp/Arrays/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.yöntem
+            string[] students = new string[3] { "Ömer", "Faruk", "Özmen" };
+
+            //2.yöntem
+            students[0] = "Ömer";
+            students[1] = "Faruk";
+            students[2] = "Özmen";
+
+            //3.yöntem
+            string[] students2 = { "Ömer", "Faruk", "Özmen" };
+
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+            foreach (var student in students2)
+            {
+                Console.WriteLine(student);
+            }
+
+
+            Console.WriteLine("--------------------------");
+
+
+            string[,] regions = new string[5, 3]
+            {
+                {"İstanbul","İzmit","Balıkesir"},
+                { "Ankara","Konya","Niğde"},
+                { "Antalya","Adana","Mersin"},
+                { "Rize","Trabzon","Samsun"},
+                { "İzmir","Muğla","Manisa"},
+            };
+
+            for (int i = 0; i <= regions.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= regions.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(regions[i,j]);
+                }
+                Console.WriteLine("********");
+            }
+
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp/Loops/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ForLoop();
+
+            WhileLoop();
+
+            DoWhileLoop();
+
+            string[] students = new string[3] {"Ömer", "Faruk", "Özmen"};
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void DoWhileLoop()
+        {
+            int number = 10;
+            do
+            {
+                Console.WriteLine(number);
+                number--;
+            } while (number >= 11);
+        }
+
+        private static void WhileLoop()
+        {
+            int number = 0;
+            while (number <= 100)
+            {
+                Console.WriteLine("Now number is {0}", number);
+                number += 1;
+            }
+            Console.WriteLine("Finished!");
+        }
+
+        private static void ForLoop()
+        {
+            for (int i = 0; i <= 100; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Finished!");
+        }
+    }
+}

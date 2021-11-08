5de7cebf1d1641f933faf47e14d5e887347f3c0b
Omer Faruk Ozmen <omerfarukozmen0@gmail.com>
2021-11-08 05:19:14 +0300

    CSharp Private / Protected / Internal / Public

---
+++ CSharp/AccessModifiers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Customer
+    {
+        private int Id;
+        protected string Name;
+        public string City { get; set; }
+    }
+
+    class Student:Customer
+    {
+        public void Save()
+        {
+            
+        }
+    }
+
+    internal class Course
+    {
+        public string Name { get; set; }
+    }
+    public class CourseDemo
+    {
+        public string Name { get; set; }
+    }
+}

428de06016f031b4ebd96383642a6ee782790265
Omer Faruk Ozmen <omerfarukozmen0@gmail.com>
2022-02-05 19:05:24 +0300

    Recap Demo Checkerboard

---
+++ CSharp/EnginDemirogCamp/Encapsulation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.FirstName = "Ömer Faruk";
+
+            Console.WriteLine(customer.FirstName);
+
+            Console.ReadLine();
+        }
+    }
+}

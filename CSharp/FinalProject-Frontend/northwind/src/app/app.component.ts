49d63dfc6ae54b19d92542903dd3deab8442ad3c
Omer Faruk Ozmen <omerfarukozmen0@gmail.com>
2022-03-06 15:01:20 +0300

    Added components

--- CSharp/FinalProject-Frontend/northwind/src/app/app.component.ts
+++ CSharp/FinalProject-Frontend/northwind/src/app/app.component.ts
@@ -8,11 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title:string = 'northwind';
   user:string="Ömer Faruk Özmen";
-  product1:any={productId:1,productName:"Bardak",categoryId:1,unitPrice:5}
-  product2={productId:2,productName:"Laptop",categoryId:2,unitPrice:785}
-  product3={productId:3,productName:"Keyboard",categoryId:3,unitPrice:15}
-  product4={productId:4,productName:"Mouse",categoryId:3,unitPrice:25}
-  product5={productId:5,productName:"Camera",categoryId:2,unitPrice:75}
-
-  products = [this.product1,this.product2,this.product3,this.product4,this.product5]
+  
 }
